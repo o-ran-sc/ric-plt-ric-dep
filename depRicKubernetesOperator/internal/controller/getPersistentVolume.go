576a326224854756bf0ec75fcdc4eeeb2573defe
naman.gupta <naman.gupta@samsung.com>
2023-11-29 13:32:30 +0530

    Adding Persistent Volume for Alarm Manager.
    Adding Persistent Volume & PV Claim for Alarm Manager.
    
    Change-Id: I3d65782c3cd8f9310952e8eec929cbf030f73d51
    Signed-off-by: naman.gupta <naman.gupta@samsung.com>

---
+++ depRicKubernetesOperator/internal/controller/getPersistentVolume.go
@@ -0,0 +1,32 @@
+
+
+func GetPersistentVolume() []*unstructured.Unstructured {
+
+	persistentVolume1 := &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": "v1",
+			"kind":       "PersistentVolume",
+			"metadata": map[string]interface{}{
+				"labels": map[string]interface{}{
+					"type": "local",
+				},
+				"name":      "pv-ricplt-alarmmanager",
+				"namespace": "ricplt",
+			},
+			"spec": map[string]interface{}{
+				"accessModes": []interface{}{
+					"ReadWriteOnce",
+				},
+				"capacity": map[string]interface{}{
+					"storage": "100Mi",
+				},
+				"hostPath": map[string]interface{}{
+					"path": "/mnt/pv-ricplt-alarmmanager",
+				},
+				"storageClassName": "local-storage",
+			},
+		},
+	}
+
+	return []*unstructured.Unstructured{persistentVolume1}
+}
