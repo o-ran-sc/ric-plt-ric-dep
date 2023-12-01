a4b134f822620624b91d4e0f4c7f916efa4cd057
naman.gupta <naman.gupta@samsung.com>
2023-12-02 00:03:14 +0530

    Adding deletion of resources for RIC.
    Adding deletion of resources for RIC.
    
    Change-Id: I633755f9f686a13ec8a99d6f9ddfda55283bce44
    Signed-off-by: naman.gupta <naman.gupta@samsung.com>

--- depRicKubernetesOperator/internal/controller/delete_controller.go
+++ depRicKubernetesOperator/internal/controller/delete_controller.go
@@ -7,15 +7,25 @@ import (
 
 func (r *RicPlatformReconciler) DeleteAll() {
 	var err error
-	namespaceProvided := "ricxapp"
+	namespaceProvided := "ricplt"
 
-	for _, resource := range GetConfigMap() {
-		if resource.ObjectMeta.Namespace == "" {
-			resource.ObjectMeta.Namespace = namespaceProvided
+	for _, resource := range GetCustomResourceDefinition() {
+		if resource.GetNamespace() == "" {
+			resource.SetNamespace(namespaceProvided)
 		}
 		err = r.Delete(context.TODO(), resource)
 		if err != nil {
-			fmt.Println("Erorr During Deleting resource of GetConfigMap()| Error --> |", err)
+			fmt.Println("Erorr During Deleting resource of GetCustomResourceDefinition()| Error --> |", err)
+		}
+	}
+
+	for _, resource := range GetJob() {
+		if resource.GetNamespace() == "" {
+			resource.SetNamespace(namespaceProvided)
+		}
+		err = r.Delete(context.TODO(), resource)
+		if err != nil {
+			fmt.Println("Erorr During Deleting resource of GetJob()| Error --> |", err)
 		}
 	}
 
@@ -29,6 +39,46 @@ func (r *RicPlatformReconciler) DeleteAll() {
 		}
 	}
 
+	for _, resource := range GetPersistentVolume() {
+		if resource.GetNamespace() == "" {
+			resource.SetNamespace(namespaceProvided)
+		}
+		err = r.Delete(context.TODO(), resource)
+		if err != nil {
+			fmt.Println("Erorr During Deleting resource of GetPersistentVolume()| Error --> |", err)
+		}
+	}
+
+	for _, resource := range GetClusterRole() {
+		if resource.ObjectMeta.Namespace == "" {
+			resource.ObjectMeta.Namespace = namespaceProvided
+		}
+		err = r.Delete(context.TODO(), resource)
+		if err != nil {
+			fmt.Println("Erorr During Deleting resource of GetClusterRole()| Error --> |", err)
+		}
+	}
+
+	for _, resource := range GetEndpoints() {
+		if resource.GetNamespace() == "" {
+			resource.SetNamespace(namespaceProvided)
+		}
+		err = r.Delete(context.TODO(), resource)
+		if err != nil {
+			fmt.Println("Erorr During Deleting resource of GetEndpoints()| Error --> |", err)
+		}
+	}
+
+	for _, resource := range GetConfigMap() {
+		if resource.ObjectMeta.Namespace == "" {
+			resource.ObjectMeta.Namespace = namespaceProvided
+		}
+		err = r.Delete(context.TODO(), resource)
+		if err != nil {
+			fmt.Println("Erorr During Deleting resource of GetConfigMap()| Error --> |", err)
+		}
+	}
+
 	for _, resource := range GetService() {
 		if resource.ObjectMeta.Namespace == "" {
 			resource.ObjectMeta.Namespace = namespaceProvided
@@ -39,6 +89,26 @@ func (r *RicPlatformReconciler) DeleteAll() {
 		}
 	}
 
+	for _, resource := range GetRoleBinding() {
+		if resource.ObjectMeta.Namespace == "" {
+			resource.ObjectMeta.Namespace = namespaceProvided
+		}
+		err = r.Delete(context.TODO(), resource)
+		if err != nil {
+			fmt.Println("Erorr During Deleting resource of GetRoleBinding()| Error --> |", err)
+		}
+	}
+
+	for _, resource := range GetStatefulSet() {
+		if resource.ObjectMeta.Namespace == "" {
+			resource.ObjectMeta.Namespace = namespaceProvided
+		}
+		err = r.Delete(context.TODO(), resource)
+		if err != nil {
+			fmt.Println("Erorr During Deleting resource of GetStatefulSet()| Error --> |", err)
+		}
+	}
+
 	for _, resource := range GetIngress() {
 		if resource.GetNamespace() == "" {
 			resource.SetNamespace(namespaceProvided)
@@ -46,6 +116,56 @@ func (r *RicPlatformReconciler) DeleteAll() {
 		err = r.Delete(context.TODO(), resource)
 		if err != nil {
 			fmt.Println("Erorr During Deleting resource of GetIngress()| Error --> |", err)
+		}
+	}
+
+	for _, resource := range GetPersistentVolumeClaim() {
+		if resource.ObjectMeta.Namespace == "" {
+			resource.ObjectMeta.Namespace = namespaceProvided
+		}
+		err = r.Delete(context.TODO(), resource)
+		if err != nil {
+			fmt.Println("Erorr During Deleting resource of GetPersistentVolumeClaim()| Error --> |", err)
+		}
+	}
+
+	for _, resource := range GetServiceAccount() {
+		if resource.ObjectMeta.Namespace == "" {
+			resource.ObjectMeta.Namespace = namespaceProvided
+		}
+		err = r.Delete(context.TODO(), resource)
+		if err != nil {
+			fmt.Println("Erorr During Deleting resource of GetServiceAccount()| Error --> |", err)
+		}
+	}
+
+	for _, resource := range GetRole() {
+		if resource.ObjectMeta.Namespace == "" {
+			resource.ObjectMeta.Namespace = namespaceProvided
+		}
+		err = r.Delete(context.TODO(), resource)
+		if err != nil {
+			fmt.Println("Erorr During Deleting resource of GetRole()| Error --> |", err)
+		}
+	}
+
+	for _, resource := range GetSecret() {
+		if resource.ObjectMeta.Namespace == "" {
+			resource.ObjectMeta.Namespace = namespaceProvided
+		}
+		err = r.Delete(context.TODO(), resource)
+		if err != nil {
+			fmt.Println("Erorr During Deleting resource of GetSecret()| Error --> |", err)
+		}
+	}
+
+	for _, resource := range GetClusterRoleBinding() {
+		if resource.ObjectMeta.Namespace == "" {
+			resource.ObjectMeta.Namespace = namespaceProvided
+		}
+		err = r.Delete(context.TODO(), resource)
+		if err != nil {
+			fmt.Println("Erorr During Deleting resource of GetClusterRoleBinding()| Error --> |", err)
 		}
 	}
 
