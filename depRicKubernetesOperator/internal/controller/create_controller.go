f8fab00f8befcc8c3cb30988e4fdff1f098e58ae
naman.gupta <naman.gupta@samsung.com>
2023-12-02 00:00:40 +0530

    Adding creation of resources for RIC.
    Adding creation of resources for RIC.
    
    Change-Id: Ief7f6ffb3a3d8d5502fea3a641831abe617f3c43
    Signed-off-by: naman.gupta <naman.gupta@samsung.com>

--- depRicKubernetesOperator/internal/controller/create_controller.go
+++ depRicKubernetesOperator/internal/controller/create_controller.go
@@ -9,6 +9,66 @@ func (r *RicPlatformReconciler) CreateAll() {
 	var err error
 	namespaceProvided := "ricplt"
 
+	for _, resource := range GetCustomResourceDefinition() {
+		if resource.GetNamespace() == "" {
+			resource.SetNamespace(namespaceProvided)
+		}
+		err = r.Create(context.TODO(), resource)
+		if err != nil {
+			fmt.Println("Erorr During Creating resource of GetCustomResourceDefinition()| Error --> |", err)
+		}
+	}
+
+	for _, resource := range GetJob() {
+		if resource.GetNamespace() == "" {
+			resource.SetNamespace(namespaceProvided)
+		}
+		err = r.Create(context.TODO(), resource)
+		if err != nil {
+			fmt.Println("Erorr During Creating resource of GetJob()| Error --> |", err)
+		}
+	}
+
+	for _, resource := range GetDeployment() {
+		if resource.ObjectMeta.Namespace == "" {
+			resource.ObjectMeta.Namespace = namespaceProvided
+		}
+		err = r.Create(context.TODO(), resource)
+		if err != nil {
+			fmt.Println("Erorr During Creating resource of GetDeployment()| Error --> |", err)
+		}
+	}
+
+	for _, resource := range GetPersistentVolume() {
+		if resource.GetNamespace() == "" {
+			resource.SetNamespace(namespaceProvided)
+		}
+		err = r.Create(context.TODO(), resource)
+		if err != nil {
+			fmt.Println("Erorr During Creating resource of GetPersistentVolume()| Error --> |", err)
+		}
+	}
+
+	for _, resource := range GetClusterRole() {
+		if resource.ObjectMeta.Namespace == "" {
+			resource.ObjectMeta.Namespace = namespaceProvided
+		}
+		err = r.Create(context.TODO(), resource)
+		if err != nil {
+			fmt.Println("Erorr During Creating resource of GetClusterRole()| Error --> |", err)
+		}
+	}
+
+	for _, resource := range GetEndpoints() {
+		if resource.GetNamespace() == "" {
+			resource.SetNamespace(namespaceProvided)
+		}
+		err = r.Create(context.TODO(), resource)
+		if err != nil {
+			fmt.Println("Erorr During Creating resource of GetEndpoints()| Error --> |", err)
+		}
+	}
+
 	for _, resource := range GetConfigMap() {
 		if resource.ObjectMeta.Namespace == "" {
 			resource.ObjectMeta.Namespace = namespaceProvided
@@ -16,16 +76,6 @@ func (r *RicPlatformReconciler) CreateAll() {
 		err = r.Create(context.TODO(), resource)
 		if err != nil {
 			fmt.Println("Erorr During Creating resource of GetConfigMap()| Error --> |", err)
-		}
-	}
-
-	for _, resource := range getDeployment() {
-		if resource.ObjectMeta.Namespace == "" {
-			resource.ObjectMeta.Namespace = namespaceProvided
-		}
-		err = r.Create(context.TODO(), resource)
-		if err != nil {
-			fmt.Println("Erorr During Creating resource of GetDeployment()| Error --> |", err)
 		}
 	}
 
@@ -38,24 +88,7 @@ func (r *RicPlatformReconciler) CreateAll() {
 			fmt.Println("Erorr During Creating resource of GetService()| Error --> |", err)
 		}
 	}
-	for _, resource := range GetIngress() {
-		if resource.GetNamespace() == "" {
-			resource.SetNamespace(namespaceProvided)
-		}
-		err = r.Create(context.TODO(), resource)
-		if err != nil {
-			fmt.Println("Erorr During Creating resource of GetIngress()| Error --> |", err)
-		}
-	}
-	for _, resource := range GetPersistentVolume() {
-		if resource.GetNamespace() == "" {
-			resource.SetNamespace(namespaceProvided)
-		}
-		err = r.Create(context.TODO(), resource)
-		if err != nil {
-			fmt.Println("Erorr During Creating resource of GetPersistentVolume()| Error --> |", err)
-		}
-	}
+
 	for _, resource := range GetRoleBinding() {
 		if resource.ObjectMeta.Namespace == "" {
 			resource.ObjectMeta.Namespace = namespaceProvided
@@ -65,6 +98,27 @@ func (r *RicPlatformReconciler) CreateAll() {
 			fmt.Println("Erorr During Creating resource of GetRoleBinding()| Error --> |", err)
 		}
 	}
+
+	for _, resource := range GetStatefulSet() {
+		if resource.ObjectMeta.Namespace == "" {
+			resource.ObjectMeta.Namespace = namespaceProvided
+		}
+		err = r.Create(context.TODO(), resource)
+		if err != nil {
+			fmt.Println("Erorr During Creating resource of GetStatefulSet()| Error --> |", err)
+		}
+	}
+
+	for _, resource := range GetIngress() {
+		if resource.GetNamespace() == "" {
+			resource.SetNamespace(namespaceProvided)
+		}
+		err = r.Create(context.TODO(), resource)
+		if err != nil {
+			fmt.Println("Erorr During Creating resource of GetIngress()| Error --> |", err)
+		}
+	}
+
 	for _, resource := range GetPersistentVolumeClaim() {
 		if resource.ObjectMeta.Namespace == "" {
 			resource.ObjectMeta.Namespace = namespaceProvided
@@ -92,6 +146,26 @@ func (r *RicPlatformReconciler) CreateAll() {
 		err = r.Create(context.TODO(), resource)
 		if err != nil {
 			fmt.Println("Erorr During Creating resource of GetRole()| Error --> |", err)
+		}
+	}
+
+	for _, resource := range GetSecret() {
+		if resource.ObjectMeta.Namespace == "" {
+			resource.ObjectMeta.Namespace = namespaceProvided
+		}
+		err = r.Create(context.TODO(), resource)
+		if err != nil {
+			fmt.Println("Erorr During Creating resource of GetSecret()| Error --> |", err)
+		}
+	}
+
+	for _, resource := range GetClusterRoleBinding() {
+		if resource.ObjectMeta.Namespace == "" {
+			resource.ObjectMeta.Namespace = namespaceProvided
+		}
+		err = r.Create(context.TODO(), resource)
+		if err != nil {
+			fmt.Println("Erorr During Creating resource of GetClusterRoleBinding()| Error --> |", err)
 		}
 	}
 
